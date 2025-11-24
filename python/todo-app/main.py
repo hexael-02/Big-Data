a012a5646f95d2b53dcf9950f192ec065974f795
godcyber 2.0 <godcyber220@gmail.com>
2025-11-24 17:17:05 -0400

    Add files via upload

--- python/todo-app/main.py
+++ python/todo-app/main.py
@@ -7,9 +7,14 @@ url: str = os.environ.get("SUPABASE_URL")
 Key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url,Key)
 
-data = supabase.table('todos').insert({'name': 'jose'}).execute()
 
+
+# data = supabase.table('todos').insert({'name': 'jose'}).execute()
+
+data = supabase.table('todos').update({'name': 'jose'}).eq('name', 'Manuel').execute()
 
 data = supabase.table('todos').select ('*').execute()
+
+#data = supabase.table('todos').delete().eq('name', 'jose').execute()
 print(data) 
 
